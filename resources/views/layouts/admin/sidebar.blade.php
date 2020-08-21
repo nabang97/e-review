c679a6d7eeec6ce764206bd8805b7e1ce9867d9c
HafidzSI <crozzgazelle4@gmail.com>
2020-08-21 14:57:07 +0700

    template admin

---
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -0,0 +1,53 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="index.html">E-Review</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">REV</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="menu-header">Dashboard</li>
+            <li><a class="nav-link" href="#"><i class="fas fa-home"></i>
+                    <span>Dashboard</span></a></li>
+            <li class="menu-header">Pengelolaan Data</li>
+            <li class="dropdown">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-user"></i>
+                    <span>Data</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="">Tambah Data</a></li>
+                    <li><a class="nav-link" href="">List Data</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-file-alt"></i>
+                    <span>Data 2</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="#">Tambah Data</a></li>
+                    <li><a class="nav-link" href="#">List Data</a></li>
+                </ul>
+            </li>
+            <li class="menu-header">Settings</li>
+            <li class="dropdown">
+                <a href="" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-cogs"></i>
+                    <span>Menu</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="">Tambah Menu</a></li>
+                    <li><a class="nav-link" href="">Kelola Menu</a></li>
+                </ul>
+            </li>
+            <li class="menu-header">Penilaian tambahan</li>
+            <li class="dropdown">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-check-square"></i>
+                    <span>Data 4</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="">List Data</a></li>
+                </ul>
+            </li>
+            <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
+                <a href="#" class="btn btn-danger btn-lg btn-block btn-icon-split">
+                    <i class="fas fa-power-off"></i> Logout
+                </a>
+            </div>
+    </aside>
+</div>
